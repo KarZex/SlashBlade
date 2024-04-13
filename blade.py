9b603a51549df13a15b6d2d3de82ac2fc4c2a571
KuromiChan <soel9e9lsub2@gmail.com>
2024-04-13 10:54:43 +0900

    ttt

--- blade.py
+++ blade.py
@@ -43,7 +43,7 @@ saC_path = open("C.json","r")
 saC = json.load(saC_path)
 
 for row in csv_reader:
-    row_count += 1
+
     if row_count == target:
 
         ##########################
@@ -65,7 +65,8 @@ for row in csv_reader:
                 parts = row[i].split('-')
                 Enchants.append(parts[0])
                 Levels.append(parts[1])
-
+        
+        blade_effect = int(row[11])
 
         ##########################
         # Behavior Pack
@@ -82,7 +83,9 @@ for row in csv_reader:
         #melee damage
         item["minecraft:item"]["components"]["minecraft:damage"] = blade_damage
         #around damage
-        item["minecraft:item"]["events"]["zex:on_use"]["sequence"][0]["sequence"][0]["run_command"]["command"][1] = "execute @s ~~~ damage @e[r=3,family=monster] {} override entity @s".format(blade_around)
+        item["minecraft:item"]["events"]["zex:on_use"]["sequence"][0]["sequence"][0]["sequence"][0]["run_command"]["command"].append("execute @s[scores={blade=..19}] ~~~ damage @e[r=3,family=monster] 1 override entity @s")
+        item["minecraft:item"]["events"]["zex:on_use"]["sequence"][0]["sequence"][0]["sequence"][0]["run_command"]["command"].append("execute @s[scores={{blade=20..39}}] ~~~ damage @e[r=3,family=monster] {} override entity @s".format(blade_around))
+        item["minecraft:item"]["events"]["zex:on_use"]["sequence"][0]["sequence"][0]["sequence"][0]["run_command"]["command"].append("execute @s[scores={{blade=40..}}] ~~~ damage @e[r=3,family=monster] {} override entity @s".format(blade_around + 2))
         #sa
         if blade_sa =="A":
             item["minecraft:item"]["events"]["zex:on_use"]["sequence"][0]["sequence"].append(saA)
@@ -98,6 +101,9 @@ for row in csv_reader:
         with open('BP/items/blade/{}.json'.format(blade_id),'w') as f:
             json.dump(item,f,indent=4)
         
+        item_path = open("blade.json","r")
+        item = json.load(item_path)
+        
 
         #function
 
@@ -105,8 +111,10 @@ for row in csv_reader:
             f.write("\nexecute @a[hasitem={{location=slot.weapon.mainhand,item=blade:{0} }}] ~~~ function blade/{0}\n".format(blade_id))
 
         with open('BP/functions/blade/{}.mcfunction'.format(blade_id),'w') as f:
+            f.write("effect @s[scores={{blade=..19}}] weakness 1 0\n")
+            if(blade_effect == 1): f.write("effect @s[scores={{blade=40..}}] strength 1 1\n")
             for i in range(len(Enchants)):
-                f.write("enchant @s {0} {1}\n".format(Enchants[i],Levels[i]))
+                f.write("enchant @s[scores={{blade=40..}}] {0} {1}\n".format(Enchants[i],Levels[i]))
 
         ##########################
         # Resource Pack
@@ -141,6 +149,10 @@ for row in csv_reader:
         #name
         with open('RP/texts/ja_JP.lang','a') as f:
             f.write("\nitem.blade:{0}={1}".format(blade_id,blade_name))
+
+        
+    
+    row_count += 1
 
 
 ###################
